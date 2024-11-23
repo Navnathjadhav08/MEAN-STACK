cf51d11234488ba87f08032929258a045b2fbb37
navnathjadhav08 <navnathjadhav0824>
2024-11-23 18:25:44 +0530

    demo

---
+++ Angular_Projects/Demo/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FirstComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'MATI';
+}
+
+
