d330ef0d7339414997e452ba132495168dd29951
navnathjadhav08 <navnathjadhav0824>
2024-12-03 14:19:55 +0530

    -

--- Angular_Projects/WithService/src/app/app.component.spec.ts
+++ Angular_Projects/WithService/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Myproject' title`, () => {
+  it(`should have the 'WithService' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Myproject');
+    expect(app.title).toEqual('WithService');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Myproject');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WithService');
   });
 });
--- Angular_Projects/WithService/src/app/app.component.ts
+++ Angular_Projects/WithService/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InterpolationConceptComponent } from './interpolation-concept/interpolation-concept.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,InterpolationConceptComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Myproject';
+  title = 'WithService';
 }
